c9a6760144d7b4e69cab52b455b0daf3844e4d0e
Pincodeert <84202600+Pincodeert@users.noreply.github.com>
2021-07-08 21:10:34 +0200

    Add files via upload

---
+++ OpdrachtJS_006/script.js
@@ -0,0 +1,28 @@
+/*console.log("kneed bloem en water tot een samenhangde bal")*/
+
+const makingPizza = function (){
+    console.log("kneed bloem en water tot een samenhangde bal");
+    console.log("rol het deeg uit met een deegroller");
+    console.log("smeer licht tomatensaus uit over de pizza");
+    console.log("beleg pizza met ingredienten naar keuze");
+    console.log("bak de pizza in de pizza-oven");
+    console.log("eet smakelijk!!")
+}
+/* onderstaande betrof eerder opdracht, die later weer verwijderd moest worden. daarom in deze 
+block commetn gezet:
+makingPizza();
+makingPizza();
+makingPizza();*/
+
+const makingSushi = function (){
+    console.log("kook de kleefrijst");
+    console.log("snij vis en groente in kleine lange stukjes");
+    console.log("bedek siushibladen met een laagje rijst en leg daarop de groente overdwars");
+    console.log("rol de sushibladeren op tot een rol");
+    console.log("snij daarna in stukjes")
+}
+makingSushi();
+makingSushi();
+makingSushi();
+makingPizza();
+makingPizza();
